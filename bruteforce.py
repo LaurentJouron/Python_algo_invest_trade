fef75b0ef84a55352d1070728c358dccf9d53973
Laurent Jouron <jouronlaurent@hotmail.com>
2022-12-13 12:57:43 +0100

    Soutenance

--- bruteforce.py
+++ bruteforce.py
@@ -48,3 +48,71 @@ if __name__ == '__main__':
     run('action')
     # run('dataset1_Python+P7')
     # run('dataset2_Python+P7')
+
+
+def brute_force(actions, final_list=None, i: int = 0):
+    """
+    Create a lists that are optimized on the maximum investment sum.
+    :param:
+        list: action_list (name, cost, profit, performance)
+        list: final_list free
+        int: 0 at initialization and +1 at each recursion
+    :return:
+        list:  best actions lists
+    """
+    if final_list is None:
+        final_list = []
+    temporary_list: list = actions[i:]
+    while DataList.get_cost_invest(temporary_list) > MAX_EXPENDITURE:
+        temporary_list.pop()
+    final_list.append(temporary_list)
+    i += 1
+    if i < len(temporary_list):
+        brute_force(actions, final_list, i)
+    return final_list
+
+def compare_actions_list(action_list):
+    """
+    compare an action list in the best investment and profitability.
+    :param:
+        list: action_list (name, cost, profit, performance)
+    :return:
+        list:  best action list
+    """
+    final_list: list = []
+    if not final_list:
+        final_list.extend(action_list[0])
+    final_cost: float = DataList.get_cost_invest(final_list)
+    final_profit: float = round(DataList.get_profit(final_list), 2)
+    for i in range(len(action_list)):
+        cost: float = DataList.get_cost_invest(action_list[i])
+        profit: float = round(DataList.get_profit(action_list[i]), 2)
+        if cost > final_cost and profit > final_profit:
+            final_list.clear()
+            final_list.extend(action_list[i])
+    return final_list
+
+def run(self):
+    """
+    Build the query according to, calling all the preamble functions create.
+    :param:
+        str: file_name (csv)
+    :return:
+        float: investment cost
+        float: profit value
+        list: best solution
+    """
+    action_list = DataList.get_data_from_csv(self)
+    performance = DataList.add_performance(action_list)
+    sort_list_on_performance = DataList.sort_on_performance(performance)
+    brute = brute_force(sort_list_on_performance)
+    compare = compare_actions_list(brute)
+    print(f"\nTotal cost: {round(DataList.get_cost_invest(compare), 2)}€")
+    print(f"Total return: {round(DataList.get_profit(compare), 2)}€ \n")
+    print("Actions list:")
+    print(np.array(compare))
+
+if __name__ == '__main__':
+    run('action')
+    # run('dataset1_Python+P7')
+    # run('dataset2_Python+P7')
